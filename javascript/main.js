41a2c69f138c466087a753d01063867c712671e1
kpberry <kpberry11@gmail.com>
2016-01-26 20:36:03 -0500

    Animation, Styles, Layout
    Experiments with JQuery scrolling, color schemes, DOM element layouts.

--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,12 @@
 $(function() {
+    /* credit to Joseph Silber in this post:
+        http://stackoverflow.com/questions/7717527/
+        jquery-smooth-scrolling-when-clicking-an-anchor-link
+    */
+    $('a').click(function(){
+        $('html, body').animate({
+            scrollTop: $( $(this).attr('href') ).offset().top
+        }, 300);
+        return false;
+    });
 });
