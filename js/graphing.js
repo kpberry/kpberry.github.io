d54371117b1d6d8e879524557b3a0221071f6041
Kevin Berry <kpberry11@gmail.com>
2017-10-10 01:14:45 -0400

    Adds graphing for 2d autonomous systems

--- js/graphing.js
+++ js/graphing.js
@@ -4,8 +4,11 @@
 
 var graphing = {};
 
-graphing.graph = function (target, fn, xlim, ylim) {
-    fn = fn || function () {
+graphing.graph = function (target, fn_y, fn_x, xlim, ylim, euler_step, euler_steps) {
+    fn_y = fn_y || function () {
+            return 1;
+        };
+    fn_x = fn_x || function () {
             return 1;
         };
     var svg = d3.select(target);
@@ -14,13 +17,16 @@ graphing.graph = function (target, fn, xlim, ylim) {
     var width = parseInt(svg.style('width'));
     var height = parseInt(svg.style('height'));
 
+    euler_step = euler_step || 0.01;
+    euler_steps = euler_steps || 200;
+
     xlim = xlim || [-5, 5];
     ylim = ylim || [-5, 5];
 
     var x_scale = width / (xlim[1] - xlim[0]);
     var y_scale = height / (ylim[1] - ylim[0]);
 
-    var clear = function() {
+    var clear = function () {
         svg.selectAll('*').remove();
     };
 
@@ -33,10 +39,11 @@ graphing.graph = function (target, fn, xlim, ylim) {
         var data = [];
         var x_norm = width / steps / 3;
         var y_norm = height / steps / 3;
+        var t_step = Math.sqrt(x_step * x_step + y_step * y_step);
         for (var x = xlim[0]; x <= xlim[1]; x += x_step) {
             for (var y = ylim[0]; y <= ylim[1]; y += y_step) {
-                var dy = fn(x, y) * x_step * y_norm;
-                var dx = x_step * x_norm;
+                var dx = fn_x(x, y) * t_step * x_norm;
+                var dy = fn_y(x, y) * t_step * y_norm;
                 if (uniform) {
                     var mag = Math.sqrt(dy * dy + dx * dx);
                     dx = dx / mag * width / steps / 3;
@@ -78,14 +85,17 @@ graphing.graph = function (target, fn, xlim, ylim) {
 
         for (var i = 0; i < steps; i++) {
             var cur = data[i];
-            var dy;
+            var dx, dy;
             if (improved) {
-                var f = fn(cur[0], cur[1]);
-                dy = (f + fn(cur[0] + f * step, cur[1] + step)) * step / 2;
+                var fy = fn_y(cur[0], cur[1]);
+                var fx = fn_x(cur[0], cur[1]);
+                dx = (fx + fn_x(cur[0] + fx * step, cur[1] + step)) * step / 2;
+                dy = (fy + fn_y(cur[0] + fy * step, cur[1] + step)) * step / 2;
             } else {
-                dy = fn(cur[0], cur[1]) * step;
+                dx = fn_x(cur[0], cur[1]) * step;
+                dy = fn_y(cur[0], cur[1]) * step;
             }
-            data.push([cur[0] + step, cur[1] + dy, cur[0], cur[1], dy]);
+            data.push([cur[0] + dx, cur[1] + dy, cur[0], cur[1], dy]);
         }
 
         svg.selectAll('eulerapprox' + Math.round(10000 * Math.random()))
@@ -154,7 +164,7 @@ graphing.graph = function (target, fn, xlim, ylim) {
         var y = dim.bottom - evt.clientY;
         x = xlim[0] + x / x_scale;
         y = ylim[0] + y / y_scale;
-        drawEulerApprox([x, y], 200, 0.1, true);
+        drawEulerApprox([x, y], euler_steps, euler_step, true);
     };
 
     return {
