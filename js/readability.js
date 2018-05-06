a23c0d996b7c08f35aa32c8592eb137d975367b6
Kevin Berry <kpberry11@gmail.com>
2018-05-06 00:03:46 -0400

    Cleans up readability

--- js/readability.js
+++ js/readability.js
@@ -2,7 +2,7 @@ var readability = {};
 
 readability.log_row_count = 0;
 
-readability.log_value = function(title, value, color) {
+readability.log_value = function (title, value, color) {
     var log = document.getElementById('log');
 
     var row = log.insertRow(readability.log_row_count);
@@ -10,7 +10,7 @@ readability.log_value = function(title, value, color) {
     var valueCell = row.insertCell(1);
 
     titleCell.innerHTML = title;
-    if (typeof(value) === 'number') {
+    if (typeof (value) === 'number') {
         if (isNaN(value)) {
             value = 0;
         } else {
@@ -26,7 +26,7 @@ readability.log_value = function(title, value, color) {
     readability.log_row_count += 1;
 };
 
-readability.clear_log = function() {
+readability.clear_log = function () {
     var log = document.getElementById('log');
     while (log.firstChild) {
         log.removeChild(log.firstChild);
@@ -34,8 +34,8 @@ readability.clear_log = function() {
     readability.log_row_count = 0;
 };
 
-readability.analyze = function(str) {
-    str = str + " ";
+readability.analyze = function (str) {
+    str = str + ' ';
     var letters = readability.alphanum_count(str);
     var syllables = readability.syllable_count(str);
     var sentences = readability.sentence_count(str) || 1;
@@ -45,35 +45,35 @@ readability.analyze = function(str) {
 
     readability.clear_log();
 
-    readability.log_value("Flesch Reading ease",
+    readability.log_value('Flesch Reading ease',
         readability.flesch_reading_ease(words, sentences, syllables));
-    readability.log_value("Automated Readability Index",
+    readability.log_value('Automated Readability Index',
         readability.automated_readability_index(words, letters, sentences));
 
-    readability.log_value("", "");
+    readability.log_value('', '');
 
     var fgl = readability.flesch_grade_level(words, sentences, syllables);
     var gfi = readability.gunning_fog_index(words, sentences, complex_words);
     var cli = readability.coleman_liau_index(words, letters, sentences);
     var s = readability.smog(gt3_syllable_words, sentences);
 
-    readability.log_value("Flesch-Kincaid Grade Level", fgl);
-    readability.log_value("Gunning Fog Index", gfi);
-    readability.log_value("Coleman-Liau Index", cli);
-    readability.log_value("SMOG", s);
-    readability.log_value("Average Grade Level", (fgl + gfi + cli + s) / 4.0, true);
+    readability.log_value('Flesch-Kincaid Grade Level', fgl);
+    readability.log_value('Gunning Fog Index', gfi);
+    readability.log_value('Coleman-Liau Index', cli);
+    readability.log_value('SMOG', s);
+    readability.log_value('Average Grade Level', (fgl + gfi + cli + s) / 4.0, true);
 
-    readability.log_value("", "");
+    readability.log_value('', '');
 
-    readability.log_value("Letters", letters);
-    readability.log_value("Syllables", syllables);
-    readability.log_value("Sentences", sentences);
-    readability.log_value("Words", words);
-    readability.log_value("Complex words", complex_words);
-    readability.log_value("3 or More Syllable Words", gt3_syllable_words);
+    readability.log_value('Letters', letters);
+    readability.log_value('Syllables', syllables);
+    readability.log_value('Sentences', sentences);
+    readability.log_value('Words', words);
+    readability.log_value('Complex words', complex_words);
+    readability.log_value('3 or More Syllable Words', gt3_syllable_words);
 };
 
-readability.alphanum_count = function(str) {
+readability.alphanum_count = function (str) {
     var count = 0;
     for (var i = 0; i < str.length; i++) {
         if (readability.is_letter(str[i]) || readability.is_num(str[i])) {
@@ -84,7 +84,7 @@ readability.alphanum_count = function(str) {
     return count;
 };
 
-readability.simple_hash = function(str) {
+readability.simple_hash = function (str) {
     var result = 1;
     for (var i = 0; i < str.length; i++) {
         result = (result * 31 + str.charCodeAt(i)) & 0xFFFFFFFF;
@@ -92,12 +92,12 @@ readability.simple_hash = function(str) {
     return result;
 };
 
-readability.syllable_count_in_word = function(str) {
+readability.syllable_count_in_word = function (str) {
     var h = readability.simple_hash(str.toLowerCase());
     if (h in syllable_counts) {
         return syllable_counts[h];
     } else {
-        console.log('Unknown word: ' + str);
+
     }
 
     var count = 0;
@@ -114,10 +114,12 @@ readability.syllable_count_in_word = function(str) {
         }
     }
 
-    return (vowel) ? count + 1 : count;
+    count = (vowel) ? count + 1 : count;
+    console.log('Unknown word: ' + str + '. Guessed syllables: ' + count);
+    return count;
 };
 
-readability.sentence_count = function(str) {
+readability.sentence_count = function (str) {
     var count = 0;
     var sentence = false;
     var len = str.length;
@@ -136,7 +138,7 @@ readability.sentence_count = function(str) {
     return count + (sentence ? 1 : 0);
 };
 
-readability.word_count = function(str) {
+readability.word_count = function (str) {
     var count = 0;
     var word = 0;
 
@@ -152,11 +154,11 @@ readability.word_count = function(str) {
     return count;
 };
 
-readability.complex_word_count = function(str) {
+readability.complex_word_count = function (str) {
     return readability.range_syllable_word_count(str, 3, 100);
 };
 
-readability.syllable_count = function(str) {
+readability.syllable_count = function (str) {
     var count = 0;
     var word = 0;
     for (var i = 0; i < str.length; i++) {
@@ -171,7 +173,7 @@ readability.syllable_count = function(str) {
     return count;
 };
 
-readability.range_syllable_word_count = function(str, low, up) {
+readability.range_syllable_word_count = function (str, low, up) {
     var count = 0;
     var word = 0;
 
@@ -190,48 +192,48 @@ readability.range_syllable_word_count = function(str, low, up) {
     return count;
 };
 
-readability.is_vowel = function(c) {
+readability.is_vowel = function (c) {
     c = readability.to_lower(c);
     return c === 'a' || c === 'e' || c === 'i' || c === 'o' || c === 'u' || c === 'y';
 };
 
-readability.is_num = function(c) {
+readability.is_num = function (c) {
     return c >= '0' && c <= '9';
 };
 
 
-readability.is_terminator = function(c) {
+readability.is_terminator = function (c) {
     return c === '.' || c === '!' || c === '?' || c === '\n';
 };
 
-readability.is_letter = function(c) {
+readability.is_letter = function (c) {
     return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
 };
 
-readability.to_lower = function(c) {
+readability.to_lower = function (c) {
     return (c <= 'Z') ? c + 32 : c;
 };
 
-readability.flesch_reading_ease = function(words, sentences, syllables) {
+readability.flesch_reading_ease = function (words, sentences, syllables) {
     return 206.835 - 1.015 * words / sentences - 84.6 * syllables / words;
 };
 
-readability.flesch_grade_level = function(words, sentences, syllables) {
+readability.flesch_grade_level = function (words, sentences, syllables) {
     return 0.39 * words / sentences + 11.8 * syllables / words - 15.59;
 };
 
-readability.gunning_fog_index = function(words, sentences, complex_words) {
+readability.gunning_fog_index = function (words, sentences, complex_words) {
     return 0.4 * (words / sentences + 100.0 * complex_words / words);
 };
 
-readability.coleman_liau_index = function(words, letters, sentences) {
+readability.coleman_liau_index = function (words, letters, sentences) {
     return 5.88 * letters / words - 29.6 * sentences / words - 15.8;
 };
 
-readability.automated_readability_index = function(words, letters, sentences) {
+readability.automated_readability_index = function (words, letters, sentences) {
     return 4.71 * letters / words + 0.5 * words / sentences - 21.43;
 };
 
-readability.smog = function(complex_words, sentences) {
+readability.smog = function (complex_words, sentences) {
     return 1.043 * Math.sqrt(complex_words * 30.0 / sentences) + 3.1291;
 };
