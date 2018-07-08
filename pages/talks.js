f66d7b8ac51ef9053e8fff5f4198edee46f2ba32
Kevin Berry <kpberry11@gmail.com>
2018-07-08 16:25:40 -0400

    Adds links to talks

--- pages/talks.js
+++ pages/talks.js
@@ -5,7 +5,7 @@ var talks = [{
         "text": "Slides"
     },
     "info": [
-        "Prepared for Georgia Tech's Theory Club in Fall 2017",
+        "Prepared for Georgia Tech's [Theory Club](https://theoryclub.github.io/) in Fall 2017",
         "Details the design and implementation of a secure cryptosystem according to the 2016 NSA CNSA Suite",
         "Explains a cryptosystem as solving the problem of providing secret communication among a pair of entities who want to verify message integrity",
         "Introduces basic, insecure encryption, decryption, key exchange, and integrity algorithms, then examines in detail Diffie-Hellman-Merkle key exchange, AES encryption and decryption, and SHA-3 hashing and authentication"
@@ -17,7 +17,7 @@ var talks = [{
         "text": "Slides"
     },
     "info": [
-        "Prepared for Georgia Tech's AI club in Spring 2018",
+        "Prepared for Georgia Tech's [AI club](https://gtagency.github.io/) in Spring 2018",
         "Introduces the semi-supervised and multi-task learning problems in relation to each other and other machine learning domains",
         "Examines the strengths and weaknesses of a selection of semi-supervised and multi-task models and approaches, including ladder networks, semi-supervised expectation maximization, and transductive SVMs"
     ]
@@ -28,7 +28,7 @@ var talks = [{
         "text": "Slides"
     },
     "info": [
-        "Prepared for Georgia Tech's Theory Club in Spring 2018",
+        "Prepared for Georgia Tech's [Theory Club](https://theoryclub.github.io/) in Spring 2018",
         "Introduces standard automata in the context of computation models and language theory, including DFAs and regular languages; PDAs and context free grammars; and Turing machines and decidable languages",
         "Explains Turing completeness and the process of reducing a machine to a Turing machine to demonstrate Turing completeness",
         "Examines many surprisingly Turing complete machines, including cellular automata, Java generics, the x86 Memory Management Unit, Dwarf Fortress, and Power point"
