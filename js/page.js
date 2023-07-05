0778931ca68adfaf5c0dce68ac1c7fbdf4ee86a6
kpberry <kpberry11@gmail.com>
2023-07-05 01:16:55 -0400

    Move list element creation to page.js

--- js/page.js
+++ js/page.js
@@ -28,3 +28,44 @@ page.prepend_navigation = function (parent, color) {
 
     parent.insertBefore(nav, parent.children[0]);
 }
+
+page.make_list_elements = function (items) {
+    return items.map(({ name, info, link }) => {
+        let item_element = document.createElement("div");
+        item_element.classList.add("w3-panel");
+
+        let item_list_element = document.createElement("ul");
+        item_list_element.classList.add("w3-ul");
+        
+        let name_element;
+        if (link === undefined) {
+            name_element = document.createElement("div");
+            name_element.classList.add("w3-text-indigo");
+        } else {
+            name_element = document.createElement("a");
+            name_element.classList.add("w3-text-indigo", "w3-hover-text-black");
+            name_element.href = link;
+        }
+        name_element.innerHTML = name;
+
+        let header = document.createElement("h5");
+        header.appendChild(name_element);
+        item_element.appendChild(header);
+
+        let info_elements = info.map(line => {
+            line = line.replace(
+                /\[(.*?)\]\((.*?)\)/g,
+                '<a href="$2" target="_blank" class="w3-hover-text-indigo">$1</a>'
+            );
+
+            let li = document.createElement("li");
+            li.innerHTML = line;
+            return li;
+        });
+
+        info_elements.forEach(element => item_list_element.appendChild(element));
+        item_element.appendChild(item_list_element);
+
+        return item_element;
+    });
+};
